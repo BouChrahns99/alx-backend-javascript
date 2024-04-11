ed65516e8f9c81a3ac359b41587138f1c8bbc657
BouChrahns99 <bouchrahannnasi5@gmail.com>
2024-04-11 14:10:54 -0700

    ES6

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,28 @@
+
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"0-promise.js" [New File]                                     0,0-1         All
+
